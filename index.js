8565b89546a6dcf5b23cd83df7f5715f520f9d70
ThomasBromehead <t_bromehead@yahoo.fr>
2018-05-28 21:51:45 +0200

    Added json stringify and parse and stored data into variables

--- index.js
+++ index.js
@@ -8,6 +8,29 @@ $(document).ready(function(){
          var latitude = currentPosition.coords.latitude;
          var longitude = currentPosition.coords.longitude;
          console.log(latitude, longitude);
+         var url = 'https://api.apixu.com/v1/current.json?key=bcaf6e0ad3c94a37a1a192107182805&q=';
+         $.getJSON (url + latitude + ',' + longitude, function(data){
+            console.log(data);
+            //JSON.stringify turns a JS object into JSON text and stores that JSON text in a string.
+            var data = JSON.stringify(data);
+            //JSON.parse turns a string of JSON text into a JS object
+            var json = JSON.parse(data);
+
+            var country = json.location.country;
+            var city = json.location.name;
+            
+            var temp = json.current.temp_c;
+            var tempf = json.current.temp_f;
+            var last-update = json.current.last_update.replace('-', ' ');
+
+            var wind = json.current.wind_kph;
+            var humidity = json.current.humidity;
+            var time = json.location.localtime.split(' ')[1];
+            var cloud = json.current.cloud;
+         })
       });
    }
 })
+
+ 
+//https://api.apixu.com/v1/current.json?key=bcaf6e0ad3c94a37a1a192107182805&q=Paris
